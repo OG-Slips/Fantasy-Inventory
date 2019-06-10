b6f2588fb02d710ceec902f12d6b3a8415f62dfc
OG-Slips <51678719+OG-Slips@users.noreply.github.com>
2019-06-10 13:07:33 -0700

    Add files via upload

--- fantasyinventory.py
+++ fantasyinventory.py
@@ -1,7 +1,7 @@
 # create fantasy inventory system with dictionaries
 
-
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
 
 def displayInventory(inventory):
     print()
@@ -14,5 +14,46 @@ def displayInventory(inventory):
         else:
             item_total = 0
         print(k + ': ' + str(item_total))
-        
-displayInventory(stuff)  
+
+
+def addItem(storage):
+    displayInventory(storage)
+    print()
+    print('Would you like to add an item to inventory?')
+    print()
+    answer = input('Y/N: ')
+    if answer == 'Y':
+        print('What would you like to add?')
+        loot = input()
+        print('How much do you have to add? (Enter negative number to remove that amount)')
+        amount = int(input())
+        if loot in storage:
+            storage[loot] += amount
+            displayInventory(storage)
+        else:
+            storage[loot] = amount
+            displayInventory(storage)
+
+    elif answer == 'N':
+        print()
+        print("Here's what you have.")
+        print()
+        displayInventory(storage)
+    else:
+        print('Please enter a valid answer.')
+
+
+addItem(stuff)
+
+while True:
+    print()
+    print("Are you done?")
+    p = input('Y/N: ')
+    if p == 'Y':
+        print('Goodbye.')
+        sys.exit()
+    elif p == 'N':
+        addItem(stuff)
+    else:
+        print('Please enter a valid answer.')
+        print()
